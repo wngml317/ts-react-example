933aee222744afbf31f6a6ef4e4d2909bd5258f1
wngml317@naver.com <wngml317@wisenut.co.kr>
2023-05-25 15:30:41 +0900

    배포 후 refresh 404 수정

--- server/server.ts
+++ server/server.ts
@@ -3,10 +3,8 @@ import dotenv from "dotenv";
 import Router from "./router/index";
 import bodyParser from "body-parser";
 import path from "path";
-import { dbConnect } from "./connect/connect";
 
 dotenv.config();
-// dbConnect()
 
 const app = express();
 const PORT = process.env.PORT;
@@ -14,13 +12,14 @@ const PORT = process.env.PORT;
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use("/api", Router);
+
 app.use(express.static(path.join(__dirname, "../dist")));
-app.get("/api", (req, res) => {
+app.get("/*", (req, res) => {
     res.header("Access-Control-Allow-Origin", "*")
     res.sendFile(path.join(__dirname, "../dist", "index.html"));
 });  
 
-app.use("/api", Router);
 
 app.listen(PORT, () => {
     console.log(`⚡ Server is running at http://localhost:${PORT}`)
