0af7e494586c3806db69d3e6ce4718cda4158eca
wngml317@naver.com <wngml317@wisenut.co.kr>
2023-06-07 14:21:27 +0900

    웹팩 개발 모드 수정

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,5 +1,9 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const PORT = process.env.PORT;
 
 module.exports = merge(common, {
     mode: "development",
@@ -10,7 +14,7 @@ module.exports = merge(common, {
         historyApiFallback: true,   //react-router 사용 시 설정해 놓은 url 이외의 경로로 접속했을 때에도 index.html을 제공할지 결정하는 옵션 (새로고침 시 발생하는 에러 해결)
         proxy: {
             "/api": {
-                target: "http://localhost:8888",
+                target: `http://localhost:${PORT}`,
                 changeOrigin: true,
             }
         },
